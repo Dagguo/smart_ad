c16548e79462b1b771529bd549179588409166d5
guo Dag <dag.guo@kikatech.com>
2019-12-16 16:35:48 +0800

    webeya

--- dailyReportForWebeye2.sh
+++ dailyReportForWebeye2.sh
@@ -28,8 +28,7 @@ dsp_name='webeye2'
 utc_interval=0
 #list='lvguangli@pku.edu.cn,guangli.lv@kikatech.com,lvguangli.pku@gmail.com'
 #list='gaoyuan@kikatech.com,gaoyuanandy@163.com'
-#list='mia.yao@webeye.com,tina.yang@kikatech.com,gaoyuan@kikatech.com'
-list='846925213@qq.com'
+list='mia.yao@webeye.com,tina.yang@kikatech.com,gaoyuan@kikatech.com,846925213@qq.com'
 mkdir -p /home/guoda/log/dsp/
 dsp_log=/home/guoda/log/dsp/${dsp_name}_${start_date}-${end_date}.log
 python3 /home/guoda/smart_ad/dailyReportForDSPWithSum.py -s ${start_date} -e ${end_date} -d ${source} -n ${dsp_name} -u ${utc_interval} -l ${list} > ${dsp_log} 2>&1
